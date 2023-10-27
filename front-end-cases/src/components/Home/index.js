58f506901d1d4c13ca16dbb330ad5af54c3e7c59
Vitor Hugo <vitorhugo0031@outlook.com>
2023-10-27 01:22:45 -0300

    criando banners

--- front-end-cases/src/components/Home/index.js
+++ front-end-cases/src/components/Home/index.js
@@ -1,14 +1,28 @@
+import './home.css';
 
 
+function HomePage() {
 
-function HomePage(){
+    return (
+        <div className="home">
+            <figure className="banner-home">
+                <img src={require('../../Imagens/banner-teste.png')} alt="banner"/>
+            </figure>
 
-    return(
-        <div>
+            <div className="link-whats">
+                <div className="logo-whats">
+                    <img src={require('../../Imagens/logo-whats-branco.png')} alt="logo-whatsapp"/>
+                </div>
+                <div className="info-whats">
+                    <h1>Entre em contato conosco</h1>
+                    <p>Caso precise, só chamar no Whatsapp</p>
 
-            <h1>Olá,  Home Page</h1>
+                </div>
+            </div>
+
+
         </div>
- 
+
     )
 }
 
