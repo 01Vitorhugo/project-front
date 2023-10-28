1aa469dc9a699c1a6ebdc6432a90c62ef60bc10a
Vitor Hugo <vitorhugo0031@outlook.com>
2023-10-27 22:11:15 -0300

    fazendo testes no login

--- front-end-cases/src/components/Login/index.js
+++ front-end-cases/src/components/Login/index.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import './login.css';
 import { Link } from 'react-router-dom';
 
 function PageLogin() {
+    const [email, setEmail] = useState();
+    const [senha, setSenha] = useState();
+
+    function Logar(){
+        alert(`Email: ${email} \n Senha: ${senha}`);
+    }
 
     return (
         <div className="login">
@@ -14,14 +21,18 @@ function PageLogin() {
                 <label>Email</label>
                 <input type='text'
                     placeholder="Digite seu email.."
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                 />
 
                 <label>Senha</label>
                 <input type='password'
                     placeholder="Digite sua senha.."
+                    value={senha}
+                    onChange={e => setSenha(e.target.value)}
                 />
 
-                <button className="button-logar">Entrar</button>
+                <button className="button-logar" onClick={Logar}>Entrar</button>
                 <p>Não tem conta? <strong><Link to="/">Cadastra-se</Link></strong></p>
             </section>
 
