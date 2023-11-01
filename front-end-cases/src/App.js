6ece1c0e1fda9cd8ef8d9664ff80a3d1958bbf0b
Vitor Hugo <vitorhugo0031@outlook.com>
2023-10-31 22:14:27 -0300

    modificacoes

--- front-end-cases/src/App.js
+++ front-end-cases/src/App.js
@@ -13,14 +13,13 @@ function App() {
       <div className="App">
         <ToastContainer autoClose={3000} />
         <RoutsPage />
+
         <div className="whats">
           <a href="https://wa.me/11995216604"> <img src={require('./Imagens/logo-whats-branco.png')} alt="logo-whats" /></a>
         </div>
-<<<<<<< HEAD
-      
-=======
+
         <Footer/>
->>>>>>> 54827c5b26c607bed4f8aa1122c7e4b17a91c9d6
+        
       </div>
     </StatesProvider>
   );
